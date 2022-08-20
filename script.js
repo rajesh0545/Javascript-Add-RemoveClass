2459d75b0bc5726b8a55b356fc41ef1892ec2f78
rajesh <krajeshgowri7032@mail.com>
2022-08-20 22:24:00 +0530

    Javascript Class

---
+++ script.js
@@ -0,0 +1,15 @@
+// function myclass(){
+//     document.getElementById("demo").style.backgroundColor="pink";
+//    document.getElementsByClassName("ind");
+    
+//  }
+
+
+
+ function myclass(){
+  document.getElementsByTagName('h2')[0].style.color="red";
+    document.getElementsByTagName('h2')[1].style.color="red";
+    document.getElementsByTagName('h2')[2].style.color="red";
+ }
+
+
